516c23f26e7e17c3e7af11b7396f9b87e017d76b
Bennny20 <phientruong20@gmail.com>
2022-09-17 18:38:16 +0700

    update code

--- src/main/java/com/shoesStore/config/SpringSecurityInitializer.java
+++
@@ -1,8 +0,0 @@
-package com.shoesStore.config;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SpringSecurityInitializer extends AbstractSecurityWebApplicationInitializer {
-
-}
-
